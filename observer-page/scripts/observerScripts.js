9a1f9d4918f96d271c610208d9c1b0d9837b3c65
Artem <artemmuha@gmail.com>
2018-11-12 00:08:22 +0400

    First step. Add some animations, when user trying to change hero. Add fields in card. I'm done today.

--- observer-page/scripts/observerScripts.js
+++ observer-page/scripts/observerScripts.js
@@ -4,10 +4,27 @@ function onDelete() {
     });
 }
 
+function onChange() {
+    $(".navigation_bar").fadeOut(400, function (){
+        $(".change_mode").fadeIn(400)
+    });
+    $("input").toggleClass("editable_input");
+    $("input").prop("disabled", false);
+}
+
 $(".grid_item_card").click(function () {
-    if ($(".navigation_bar").css('display') === 'none') {
+    if ($(".delete_mode").css('display') !== 'none') {
         $(this).toggleClass("choosed_for_delete")
     }
+});
+
+$("#cancel_changes_btn").click(function () {
+    $(".change_mode").fadeOut(400, function (){
+        $(".navigation_bar").fadeIn(400)
+    });
+    $("input").removeClass("editable_input");
+    $("input").prop("disabled", true);
+
 });
 
 $("#cancel_delete_btn").click(function () {
@@ -16,3 +33,25 @@ $("#cancel_delete_btn").click(function () {
     });
     $(".grid_item_card").removeClass("choosed_for_delete")
 });
+
+$("#accept_changes").click(function () {
+    $(".page_center").toggleClass("hide_animation");
+    $(".header").toggleClass("hide_animation");
+    $('.modal_dialog').show();
+});
+
+$("#dialog_cancel").click(function () {
+    $(".page_center").toggleClass("hide_animation");
+    $(".header").toggleClass("hide_animation");
+    $('.modal_dialog').fadeOut(600);
+});
+
+$('#delete_selected').click(function () {
+    $(".page_center").toggleClass("hide_animation");
+    $(".header").toggleClass("hide_animation");
+    $('.modal_dialog').show();
+});
+
+$("#dialog_delete_selected").click(
+//    BACKEND
+);
