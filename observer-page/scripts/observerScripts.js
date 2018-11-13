315b432b9c52efc6939539972d79d0edb290b2d3
Artem <artemmuha@gmail.com>
2018-11-13 21:55:04 +0400

    First step. Add search field.

--- observer-page/scripts/observerScripts.js
+++ observer-page/scripts/observerScripts.js
@@ -1,19 +1,31 @@
 var isDeleteMode = false;
 
 function onLoad() {
-    $( "input" ).prop( "disabled", true );
+    $("input:not(#search)").prop("disabled", true);
 }
 
+$("#search").keyup(function () {
+    let _this = this;
+    $.each($(".card_name"), function () {
+        if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
+            $(this).closest(".grid_item_card").hide();
+        } else {
+            $(this).closest(".grid_item_card").show();
+        }
+    })
+});
+
 function onDelete() {
-    $(".navigation_bar").fadeOut(400, function (){
+    $(".navigation_bar").fadeOut(400, function () {
         $(".delete_mode").fadeIn(400)
     });
     isDeleteMode = true;
     $(".head_image").hide();
+    $("#new_hero_card").fadeOut(400);
 }
 
 function onAddHero() {
-    $(".navigation_bar").fadeOut(400, function (){
+    $(".navigation_bar").fadeOut(400, function () {
         $(".add_mode").fadeIn(400)
     });
     $(".page_center").fadeOut(400, function () {
@@ -23,22 +35,21 @@ function onAddHero() {
     });
 }
 
-$(".head_change_pencil").click(function () {
-    $(".navigation_bar").fadeOut(400, function (){
+$("div").on('click', ".head_change_pencil", function () {
+    $(".navigation_bar").fadeOut(400, function () {
         $(".change_mode").fadeIn(400)
     });
+
+    $("#new_hero_card").fadeOut(400);
+
     $(".head_change_pencil").hide();
     // Ужасный поиск нужных инпутов.
     let input_tags = $(this).parent("div").siblings(".hero_desc").children("label").children("input");
     input_tags.toggleClass("editable_input");
-    if (input_tags.prop("disabled") === false) {
-        input_tags.prop("disabled", true)
-    } else {
         input_tags.prop("disabled", false);
-    }
 });
 
-$(".grid_item_card").click(function () {
+$("div").on('click', ".grid_item_card", function () {
     if (isDeleteMode) {
         $(this).toggleClass("choosed_for_delete")
     }
@@ -47,7 +58,7 @@ $(".grid_item_card").click(function () {
 $("#cancel_adding_btn").click(function () {
     $("input").prop("disabled", true);
     $("input").removeClass("editable_input");
-    $(".add_mode").fadeOut(400, function (){
+    $(".add_mode").fadeOut(400, function () {
         $(".navigation_bar").fadeIn(400)
     });
     $(".page_center_addhero").fadeOut(400, function () {
@@ -58,25 +69,32 @@ $("#cancel_adding_btn").click(function () {
 $("#cancel_changes_btn").click(function () {
     $("input").prop("disabled", true);
     $("input").removeClass("editable_input");
-    $(".change_mode").fadeOut(400, function (){
+    $(".change_mode").fadeOut(400, function () {
         $(".navigation_bar").fadeIn(400)
     });
     $(".head_change_pencil").show();
+    $("#new_hero_card").fadeIn(400);
 });
 
 $("#cancel_delete_btn").click(function () {
     isDeleteMode = false;
     $(".grid_item_card").removeClass("choosed_for_delete");
-    $(".delete_mode").fadeOut(400, function (){
+    $(".delete_mode").fadeOut(400, function () {
         $(".navigation_bar").fadeIn(400)
     });
     $(".head_image").show();
+    $("#new_hero_card").fadeIn(400);
 });
 
 $("#accept_changes").click(function () {
-    $(".page_center").toggleClass("hide_animation");
-    $(".header").toggleClass("hide_animation");
-    $('.modal_dialog').show();
+    $("input").removeClass("editable_input");
+    $("input").prop("disabled", true);
+
+    $(".change_mode").fadeOut(400, function () {
+        $(".navigation_bar").fadeIn(400)
+    });
+    $(".head_change_pencil").show();
+    $("#new_hero_card").fadeIn(400);
 });
 
 $("#dialog_cancel").click(function () {
@@ -91,6 +109,92 @@ $('#delete_selected').click(function () {
     $('.modal_dialog').show();
 });
 
-$("#dialog_delete_selected").click(
-//    BACKEND
-);
+$("#dialog_accept").click(function () {
+    $(".choosed_for_delete").hide();
+    $("#dialog_cancel").click();
+    $("#cancel_delete_btn").click();
+});
+
+$("#add_card_btn").click(function () {
+    let message_box = $("#message");
+    let heroname = $("#add_param_heroname").val();
+    let file = $("#fileloader").prop('files')[0];
+    let universe = $("#add_param_universe").val();
+    let power = $("#add_param_power").val();
+    let desc = $("#add_param_desc").val();
+    let isAlive = $("#add_param_checkbox").val();
+    let phone = $("#add_param_phone").val();
+
+    message_box.text("");
+    if (!(/^\w+$/.test(heroname))) {
+        message_box.append("Please, enter the correct hero name!<br><br>");
+    }
+    if (file != null) {
+        if (!(file.size > 0 && file.size < 2048000)) {
+            message_box.append("File is too big ( bigger than 2048 bites ), or too small!<br><br>");
+        }
+        if (file.type !== "image/png" && file.type !== "image/jpeg") {
+            message_box.append("You're trying to upload not image file!<br><br>");
+        }
+    }
+
+    if (!(/^[a-zA-Z]+$/.test(universe))) {
+        message_box.append("Incorrect universe name<br><br>");
+    }
+    if (!(power >= 0 && power <= 100)) {
+        message_box.append("Incorrect power value<br><br>");
+    }
+    if (phone !== "" && !(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(phone))) {
+        message_box.append("You'ra entered phone number with unknown format. (Example: 89272151817)<br><br>")
+    }
+
+    if (message_box.text() === "" && heroname.length !== 0) {
+        if (!check_name(name)) {
+            message_box.append("Hero with this name is exist<br><br>");
+        }
+        let new_id = "card_" + ($(".card").length + 1);
+        // Скачать картинку на сервер!
+        let filename = "img/unknown_hero.png";
+        let isalive = "checked" ? isAlive : false;
+        create_new_card(new_id, heroname, filename, universe, power, desc, isalive, phone);
+        $("#cancel_adding_btn").click();
+    }
+});
+
+function create_new_card(id, name, file, universe, power, desc, isAlive, phone) {
+    $("#new_hero_card").before(
+        "<div class=\"grid_item_card\">\n" +
+        "            <article id=\"card_" + id + "\" class=\"card\">\n" +
+        "                <div class=\"card_head\">\n" +
+        "                    <span class=\"card_name\">" + name + "</span>\n" +
+        "                    <a class=\"head_change_pencil\" onclick=\"\"><img class=\"head_image\" src=\"img/pencil.png\"></a>\n" +
+        "                </div>\n" +
+        "                <div class=\"hero_image_href\">\n" +
+        "                    <img class=\"hero_image\" alt=\"" + name + "\" src=\"" + file + "\">\n" +
+        "                </div>\n" +
+        "                <div class=\"hero_desc\">\n" +
+        "                    <label>\n" +
+        "                        <strong>Universe</strong>\n" +
+        "                        <input type=\"text\" class=\"universe_input\" value=\"" + universe + "\">\n" +
+        "                    </label>\n" +
+        "                    <label>\n" +
+        "                        <strong>Power</strong>\n" +
+        "                        <input type=\"number\" class=\"power_input\" value=\"" + power + "\">\n" +
+        "                    </label>\n" +
+        "                    <label>\n" +
+        "                        <strong>Desc</strong>\n" +
+        "                        <input type=\"text\" class=\"desc_input\" value=\"" + desc + "\">\n" +
+        "                    </label>\n" +
+        "                    <label>\n" +
+        "                        <strong>Alive</strong>\n" +
+        "                        <input type=\"checkbox\" class=\"is_alive_input\" " + isAlive + ">\n" +
+        "                    </label>\n" +
+        "                    <label>\n" +
+        "                        <strong>Phone</strong>\n" +
+        "                        <input type=\"text\" class=\"phone_input\" value=\"" + phone + "\">\n" +
+        "                    </label>\n" +
+        "                </div>\n" +
+        "            </article>\n" +
+        "        </div>"
+    );
+}
